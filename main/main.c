fc1393ba1daf8a1df206a95ac6a1d7b1c7f1eb0b
dominik9 <dominik3gumis@gmail.com>
2021-07-27 17:58:51 +0200

    To master

--- main/main.c
+++ main/main.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include "ledsControl.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 //Działanie aplikacji:
 //Przyciski i ledy są przypisane do odpowiednich numerów
 //Jeżeli został przyciśniety przycisk pod numerem jakimś to led zmienia tryb działania
 //Tryby działania: Stały, pulsacja, miganie, wyłączenie
-
+void ledTask(void * param){
+  printf("Start task led\n");
+  initLedsFromFileIni();
+  changeStage(2);
+  while (1){
+    changeStage(0);
+    changeStage(1);
+    changeStage(2);
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
+  }
+}
 
 void app_main(void)
 {
   printf("Hello world!\n");
-  initLedsFromFileIni();
-  changeStage(3);
+  xTaskCreate(&ledTask, "led task", 40048, "ledTask", 2, NULL);
+  
 }
