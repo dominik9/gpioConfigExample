024955bd019068ae2b6f9e797f758237f4bac75a
dominik9 <dominik3gumis@gmail.com>
2021-07-27 17:55:03 +0200

    Test led

--- components/LedExample/include/buttonsControl.h
+++ components/LedExample/include/buttonsControl.h
@@ -1,4 +1,12 @@
 //Tak jak led control
 //Przerwania
 //Extern jako kolejka
-//
+
+#ifndef _BUTTONS_CONROL_H
+#define _BUTTONS_CONROL_H
+
+#include <stdint.h>
+
+#define NUMBER_OF_BUTTONS  3
+
+#endif
